c6f839e291db82c1886ba4403958fd279842fef7
Aleksandr.Kashirin <saniok26@rambler.ru>
2022-05-31 01:44:18 +0300

    working process

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,14 @@
+import { NavLink } from 'react-router-dom';
+import styles from './Navigation.module.css';
+
+export function Navigation() {
+    return (
+        <>
+            <nav className={styles.navigation}>
+                <NavLink to='/' className={styles.navLink}>Home</NavLink>
+                <NavLink to='/movies' className={styles.navLink}>Movies</NavLink>
+            </nav>
+            <hr style={{boxShadow: "0px 3px 10px 1px var(--brand-color)", width: "90%"}}></hr>
+        </>
+    )
+}  
---
+++ src/services/fetch-api.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://api.themoviedb.org/3/movie";
+
+export async function getTranding() {
+    // const response = await axios.get("")
+}
