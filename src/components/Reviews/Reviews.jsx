bffc7c8b69ee7f6cefb5e1b15e108c8b3ab4b03d
Aleksandr.Kashirin <saniok26@rambler.ru>
2022-06-04 02:12:23 +0300

    reviews

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -6,31 +6,50 @@ import styles from './Reviews.module.css';
 
 
 export function Reviews() {
-    const [data, setData] = useState(null);
+    const [reviews, setReviews] = useState(null);
     const movieId = useParams().movieId;
 
     useEffect(() => {
-        API.getReviewsById(movieId).then(setData);
+        API.getReviewsById(movieId).then((data)=> setReviews(data.results));
     }, [movieId])
     
-    console.log(data)
+    console.log(reviews );
 
-    // data.results === [] ? 
-    return ( data &&
-        <ul className={styles.cardList}>
-            {data.results.map(({ id, author, content, author_details: { avatar_path: avatar } }) => {
-                console.log(avatar.slice(1,6))
-                return <li key={id}>
-                            <div className={styles.cardCast}>
-                                <img className={styles.avatar} src={avatar.slice(1, 6) === "https"
-                                    ? avatar.slice(1, avatar.length)
-                                    : `${imageDefaultLink}${avatar}`}
-                                    alt={author} />
-                                <h3>{author}</h3>
-                                <p>{content}</p>
-                            </div>
-                        </li>
-            })}
-        </ul>
-        )
+    return  (reviews?.length > 0
+            ? <ul className={styles.cardList}>
+            {reviews.map(({ id, author, content, author_details: { avatar_path: avatar } }) => {     
+                    console.log(avatar)
+                    return <li key={id}>
+                                <div className={styles.cardReview}>
+                                    <div className={styles.authorReview}>
+                                        <img className={styles.avatar} src={avatar?.slice(1, 6) === "https"
+                                            ? avatar.slice(1, avatar.length)
+                                            : `${imageDefaultLink}${avatar}`}
+                                            alt={author} />
+                                        <h3>{author}</h3>
+                                    </div>
+                                    <p>{content}</p>
+                                </div>
+                            </li>
+                })}
+            </ul>
+            : <div>No reviews</div>)
+        
+    // return ( data &&
+    //     <ul className={styles.cardList}>
+    //         {data.results.map(({ id, author, content, author_details: { avatar_path: avatar } }) => {
+    //             console.log(avatar.slice(1,6))
+    //             return <li key={id}>
+    //                         <div className={styles.cardCast}>
+    //                             <img className={styles.avatar} src={avatar.slice(1, 6) === "https"
+    //                                 ? avatar.slice(1, avatar.length)
+    //                                 : `${imageDefaultLink}${avatar}`}
+    //                                 alt={author} />
+    //                             <h3>{author}</h3>
+    //                             <p>{content}</p>
+    //                         </div>
+    //                     </li>
+    //         })}
+    //     </ul>
+    //     )
 }
